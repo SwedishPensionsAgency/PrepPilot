54b9e0c83eabe0c38da7a0a34d69de54444ba868
Love Hansson <love.hansson@pensionsmyndigheten.se>
2014-05-20 16:06:13 +0200

    Add dependencies, clean up base data

--- global.R
+++ global.R
@@ -4,15 +4,16 @@
 require(shiny) # >= 0.9
 require(Coldbir) # >= 0.5
 require(data.table) # >= 1.9.2¨
-
+require(ggplot2)
+require(sparkle)
 
 ## Data ----
 prepDB <- cdb("Data//DataBas")
 
 system.time(base_data <- data.table(
-  FODAR = prepDB['FODAR'],
-  FONDPLACERINGSAR = prepDB['FONDPLACERINGSAR'],
-  INTJANANDEAR = prepDB['INTJANANDEAR'],
-  INTRADESAR = prepDB['INTRADESAR'],
-  SEX = prepDB['SEX']  
+  prepDB['FODAR'],
+  prepDB['FONDPLACERINGSAR'],
+  prepDB['INTJANANDEAR'],
+  prepDB['INTRADESAR'],
+  prepDB['SEX']
 ))
