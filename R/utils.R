668d54b4d0ee568e23cc9bcbb9c7479288ae474f
Love Hansson <love.hansson@pensionsmyndigheten.se>
2014-07-16 12:47:48 +0200

    Remove accidental testing code

--- R/utils.R
+++ R/utils.R
@@ -10,6 +10,7 @@ Run <- function(app = NULL) {
   runApp(".")
 }
 
+
 #' Create a partitioned frequency table from panel data
 #' 
 #' @export
@@ -21,11 +22,15 @@ partTable <- function(data, byvar = NULL, grpvar = NULL, freqvar = NULL, sumname
     summarise(freq = n()) %>%
     spread_(key_col = byvar, "freq", fill = 0)
   
+  
+  ## Add totals column
+  
   # If no "grpvar" is supplied, i.e. if the return data should have only one row,
   # then partTable returns a table that must be flattened before being returned.
   if (is.null(grpvar)) {
     freqtbl <- colwise(sum, na.rm = TRUE)(freqtbl)
     freqtbl$tot <- sum(freqtbl)
+    freqtbl <- freqtbl[, c(ncol(freqtbl), 1:(ncol(freqtbl)-1))]
     
   } else {
     freqtbl <- data %>%
@@ -38,11 +43,3 @@ partTable <- function(data, byvar = NULL, grpvar = NULL, freqvar = NULL, sumname
   
   return(freqtbl)
 }
-
-
-
-mfdata <- tbl_df(data.frame(
-  kon = letters[sample(c(6, 13, 15), 50, replace = TRUE)],
-  someval = runif(50),
-  utb = sample(1:2, 50, replace = TRUE)
-))
