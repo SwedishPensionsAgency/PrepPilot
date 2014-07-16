a3349e12f9d1e6622598db25486aaa9c278537a0
Love Hansson <love.hansson@pensionsmyndigheten.se>
2014-07-16 13:29:18 +0200

    Completed work on partTable()

--- R/utils.R
+++ R/utils.R
@@ -16,30 +16,39 @@ Run <- function(app = NULL) {
 #' @export
 
 partTable <- function(data, byvar = NULL, grpvar = NULL, freqvar = NULL, sumname = "total") {
-  freqtbl <- data.frame(data) %>%
-    tbl_df() %>%
-    regroup(as.list(c(unlist(byvar), unlist(grpvar)))) %>%
-    summarise(freq = n()) %>%
-    spread_(key_col = byvar, "freq", fill = 0)
+  # Start with totals column
+  maindata <- data %>%
+    regroup(as.list(grpvar)) %>%
+    summarise("tot" = n())
   
-  
-  ## Add totals column
-  
-  # If no "grpvar" is supplied, i.e. if the return data should have only one row,
-  # then partTable returns a table that must be flattened before being returned.
-  if (is.null(grpvar)) {
-    freqtbl <- colwise(sum, na.rm = TRUE)(freqtbl)
-    freqtbl$tot <- sum(freqtbl)
-    freqtbl <- freqtbl[, c(ncol(freqtbl), 1:(ncol(freqtbl)-1))]
-    
-  } else {
-    freqtbl <- data %>%
-      regroup(as.list(grpvar)) %>%
-      summarise("tot" = n()) %>%
-      left_join(freqtbl, by = grpvar)
+  # Add columns for each of the by values in each of the by variables
+  for (var in byvar) {
+    if (!is.null(grpvar)) {
+      freqtbl <- data.frame(data) %>%
+        tbl_df() %>%
+        regroup(as.list(c(var, unlist(grpvar)))) %>%
+        summarise(freq = n()) %>%
+        spread_(key_col = var, "freq", fill = 0)
+      
+      maindata <- maindata %>% 
+        left_join(freqtbl, by = grpvar)
+    } else {
+      # If no 'grpvar' is supplied, we want end up with a one-row data frame
+      freqvec <- data %>%
+        tbl_df() %>%
+        regroup(as.list(var)) %>%
+        summarise(freq = n()) %>%
+        t() %>%
+        as.data.frame(., stringsAsFactors = FALSE)
+      
+      setnames(freqvec, names(freqvec), as.character(freqvec[1,]))
+      freqvec <- freqvec[2,] %>% mutate_each(., funs(as.integer))
+      maindata <- cbind(maindata, freqvec)
+    }
   }
   
-  setnames(freqtbl, "tot", sumname)
-  
-  return(freqtbl)
+  # Rename totals column and return
+  setnames(maindata, "tot", sumname)
+  return(maindata)
 }
+
